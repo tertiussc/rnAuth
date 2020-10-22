2efccc68cb1ab9ca07e9472ddf26f6bc70cfe203
Tertius Scheepers <Tertiussc@gmail.com>
2020-10-22 09:51:55 +0200

    completed lesson 319: also not using quick import trick

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Header = ({ headerText }) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>{headerText}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        // justifyContent is to position vertically and alignItem is for Horizontal
+        backgroundColor: '#f2f2f2',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+
+        // Shaddow
+        shadowColor: '#000',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.2,
+        // Android specific fix for shaddow
+        elevation: 2,
+        position: 'relative'
+    },
+    header: {
+        fontSize: 28,
+    }
+})
+
+export default Header;
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Header from '../components/Header'
 
 const SignInScreen = () => {
     const { mainText } = styles
 
 
     return (
-        <View>
+        <View style={{ marginTop: 15 }}>
+            <Header headerText="Authentication" />
             <Text style={mainText}>Sign In/Up Screen</Text>
-        </View>
+        </View >
     );
+}
+
+SignInScreen.navigationOptions = () => {
+    return {
+        header: () => false
+    }
 }
 
 const styles = StyleSheet.create({
