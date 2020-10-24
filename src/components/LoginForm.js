e3c36f72be993d3768a108951c836ac754a00653
Tertius Scheepers <Tertiussc@gmail.com>
2020-10-24 07:57:25 +0200

    completed lesson 329: Login form built

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+
 // custom components
 import Card from './Card'
 import CardSection from './CardSection'
 import Button from './Button'
+import Input from './Input'
 
 class LoginForm extends Component {
-
+    state = { email: '', password: '' }
     render() {
         return (
             <Card>
                 <CardSection>
-
+                    <Input
+                        label="Email"
+                        placeholder="user@email.com"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                    />
                 </CardSection>
                 <CardSection>
-
+                    <Input
+                        label="Password"
+                        placeholder="password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        secureTextEntry
+                    />
                 </CardSection>
                 <CardSection>
                     <Button>
