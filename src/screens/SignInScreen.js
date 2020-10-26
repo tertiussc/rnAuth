de9b8aa89514a78129be88f8016f7492b02a0895
Tertius Scheepers <Tertiussc@gmail.com>
2020-10-26 08:13:02 +0200

    completed lesson 337: added logout button and case statement => firebase still not working

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -4,8 +4,12 @@ import firebase from 'firebase'
 // Custom imports
 import Header from '../components/Header'
 import LoginForm from '../components/LoginForm'
+import Button from '../components/Button';
+import Spinner from '../components/Spinner'
 
 class SignInScreen extends Component {
+
+    state = { loggedIn: null }
 
     componentDidMount() {
         firebase.initializeApp({
@@ -18,13 +22,34 @@ class SignInScreen extends Component {
             appId: "1:553481766548:web:d295623fcbd31458e31681",
             measurementId: "G-1LRQP3YPLW"
         })
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true })
+            } else {
+                this.setState({ loggedIn: false })
+            }
+        })
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return <Button>Log Out</Button>
+            case false:
+                return <LoginForm />
+            default:
+                return <View style={styles.spinnerContainerStyle}>
+                    <Spinner size="large" />
+                </View>
+        }
     }
 
     render() {
         return (
             <View style={{ marginTop: 15 }}>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View >
         );
     }
@@ -37,6 +62,11 @@ SignInScreen.navigationOptions = () => {
 }
 
 const styles = StyleSheet.create({
+    spinnerContainerStyle: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 export default SignInScreen;
